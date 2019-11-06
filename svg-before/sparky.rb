90cccc686f896ca990b39b84123e9e7ec4609b68
Kirsten Nordine <kirstennordine@gmail.com>
2019-11-05 22:15:34 -0500

    Extract methods for line, polyline svg elements
    Addresses Inconsistent Style issues

--- svg-before/sparky.rb
+++ svg-before/sparky.rb
@@ -22,6 +22,14 @@ def text(center_x, center_y, value)
     fill=\"red\" >#{value}</text>"
 end
 
+def line(x1, y1, x2, y2)
+  "<line x1=\"#{x1}\" y1=\"#{y1}\" x2=\"#{x2}\" y2=\"#{y2}\" stroke=\"#999\" stroke-width=\"1\" />"
+end
+
+def polyline(points)
+  "<polyline fill=\"none\" stroke=\"#333\" stroke-width=\"1\" points = \"#{points.join(' ')}\" />"
+end
+
 def spark(center_x, center_y, value)
   "#{rect(center_x, center_y)}\"
    #{text(center_x, center_y, value)}"
@@ -34,10 +42,8 @@ $tosses.each_index { |i| points << "#{i},#{200-$tosses[i]}" }
 data = "<svg xmlns=\"http://www.w3.org/2000/svg\"
      xmlns:xlink=\"http://www.w3.org/1999/xlink\" >
   <!-- x-axis -->
-  <line x1=\"0\" y1=\"200\" x2=\"#{NUMBER_OF_TOSSES}\" y2=\"200\"
-            stroke=\"#999\" stroke-width=\"1\" />
-  <polyline fill=\"none\" stroke=\"#333\" stroke-width=\"1\"
-    points = \"#{points.join(' ')}\" />
+  #{line(0, 200, NUMBER_OF_TOSSES, 200)}
+  #{polyline(points)}
   #{spark(NUMBER_OF_TOSSES-1, 200-$tosses[-1], $tosses[-1])}
 </svg>"
 
