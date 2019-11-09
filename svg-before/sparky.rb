85b7189c44e788bca7e00a9e68fe2065356101aa
Kirsten Nordine <kirstennordine@gmail.com>
2019-11-09 13:35:54 -0500

    Move spark method into Sparkline object

--- svg-before/sparky.rb
+++ svg-before/sparky.rb
@@ -7,6 +7,9 @@ class Sparkline
     @y_values = y_values
   end
 
+  SQUARE_SIDE = 4
+  SQUARE_COLOR = "red"
+
   def x_axis
     "<!-- x-axis -->
     #{SVG.line(0, 200, y_values.length, 200, "#999", 1)}"
@@ -17,6 +20,16 @@ class Sparkline
     y_values.each_index { |i| points << "#{i},#{200 - y_values[i]}" }
     "<!-- sparkline -->
     #{SVG.polyline("none", "#333", 1, points)}"
+  end
+
+  def spark
+    final_value = y_values[-1]
+    center_x = y_values.length - 1
+    center_y = 200 - final_value
+    "<!-- spark -->
+    #{SVG.rect(center_x - (SQUARE_SIDE / 2), center_y - (SQUARE_SIDE / 2), SQUARE_SIDE, SQUARE_SIDE, SQUARE_COLOR, "none", 0)}\"
+    <!-- final value -->
+    #{SVG.text(center_x, center_y, "Verdana", 9, SQUARE_COLOR, final_value)}"
   end
 end
 
@@ -30,26 +43,13 @@ def values(n)
   a
 end
 
-SQUARE_SIDE = 4
-SQUARE_COLOR = "red"
-
-def spark(y_values)
-  final_value = y_values[-1]
-  center_x = y_values.length - 1
-  center_y = 200 - final_value
-  "<!-- spark -->
-  #{SVG.rect(center_x - (SQUARE_SIDE / 2), center_y - (SQUARE_SIDE / 2), SQUARE_SIDE, SQUARE_SIDE, SQUARE_COLOR, "none", 0)}\"
-  <!-- final value -->
-  #{SVG.text(center_x, center_y, "Verdana", 9, SQUARE_COLOR, final_value)}"
-end
-
 sp = Sparkline.new(values(1000))
 
 data = "<svg xmlns=\"http://www.w3.org/2000/svg\"
      xmlns:xlink=\"http://www.w3.org/1999/xlink\" >
   #{sp.x_axis}
   #{sp.sparkline}
-  #{spark(sp.y_values)}
+  #{sp.spark}
 </svg>"
 
 puts "Content-Type: image/svg+xml
