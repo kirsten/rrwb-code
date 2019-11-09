d45de70eb67eb037bc449aa49d96876bfa1d3c76
Kirsten Nordine <kirstennordine@gmail.com>
2019-11-09 14:58:25 -0500

    Add simple rectangle class with message chain code smell
    There isn't a real code example in the book/codebase, so just copying
    some of the basics.

---
+++ rectangle/rectangle.rb
@@ -0,0 +1,28 @@
+class Rectangle
+  attr_reader :top_left, :width, :height
+
+  def initialize(point, width, height)
+    @top_left = point
+    @width = width
+    @height = height
+  end
+end
+
+class Point
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+end
+
+point = Point.new(5, 7)
+rect = Rectangle.new(point, 20, 30)
+
+puts point
+puts rect
+
+# Want to avoid rectangle having knowledge of the implementation of point when
+# finding the x-coordinate of the rectangle's left edge.
+puts rect.top_left.x
