fcac21e906a9d5ee663ad3575a9d1b0eb35cef4d
Kirsten Nordine <kirstennordine@gmail.com>
2019-11-09 14:17:00 -0500

    Make height_above_x_axis private getter

--- svg-before/sparkline.rb
+++ svg-before/sparkline.rb
@@ -10,7 +10,7 @@ class Sparkline
   def to_svg
     "<svg xmlns=\"http://www.w3.org/2000/svg\"
          xmlns:xlink=\"http://www.w3.org/1999/xlink\" >
-      <g transform=\"translate(0, #{@height_above_x_axis})\">
+      <g transform=\"translate(0, #{height_above_x_axis})\">
         #{x_axis}
         #{sparkline}
         #{spark}
@@ -18,13 +18,9 @@ class Sparkline
     </svg>"
   end
 
-  def reflect_top_and_bottom(y_values)
-    y_values.map { |y| -y }
-  end
-
   private
 
-  attr_reader :y_values
+  attr_reader :y_values, :final_value, :height_above_x_axis
 
   SQUARE_SIDE = 4
   SQUARE_COLOR = "red"
@@ -47,7 +43,12 @@ class Sparkline
     "<!-- spark -->
     #{SVG.rect(center_x - (SQUARE_SIDE / 2), center_y - (SQUARE_SIDE / 2), SQUARE_SIDE, SQUARE_SIDE, SQUARE_COLOR, "none", 0)}\"
     <!-- final value -->
-    #{SVG.text(center_x + 6, center_y + 4, "Verdana", 9, SQUARE_COLOR, @final_value)}"
+    #{SVG.text(center_x + 6, center_y + 4, "Verdana", 9, SQUARE_COLOR, final_value)}"
   end
+
+  def reflect_top_and_bottom(y_values)
+    y_values.map { |y| -y }
+  end
+
 end
 
