65906cd953ee210383fbb17f3b3eac86d737002d
Kirsten Nordine <kirstennordine@gmail.com>
2019-11-19 21:54:48 -0500

    Remove trailing whitespace from tic_tac_toe_test

--- tic_tac_toe/tic_tac_toe_test.rb
+++ tic_tac_toe/tic_tac_toe_test.rb
@@ -9,32 +9,32 @@ class GameTest < Test::Unit::TestCase
                     "OXO")
     assert_equal(5, game.move('X'))
 
-    game = Game.new("XOX" + 
-                    "OXO" + 
+    game = Game.new("XOX" +
+                    "OXO" +
                     "OX-")
     assert_equal(8, game.move('O'))
 
-    game = Game.new("---" + 
-                    "---" + 
+    game = Game.new("---" +
+                    "---" +
                     "---")
     assert_equal(0, game.move('X'))
 
-    game = Game.new("XXX" + 
-                    "XXX" + 
+    game = Game.new("XXX" +
+                    "XXX" +
                     "XXX")
     assert_equal(-1, game.move('X'))
   end
 
   def test_find_winning_move
-    game = Game.new("XO-" + 
-                    "XX-" + 
+    game = Game.new("XO-" +
+                    "XX-" +
                     "OOX")
     assert_equal(5, game.move('X'))
   end
 
   def test_win_conditions
-    game = Game.new("---" + 
-                    "XXX" + 
+    game = Game.new("---" +
+                    "XXX" +
                     "---")
     assert_equal('X', game.winner())
   end
